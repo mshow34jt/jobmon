c0f1b92cafdde7daaa1711c822fc63fbda5c8621
Mike Showerman <mshow@ncsa.illinois.edu>
2021-10-28 20:36:26 -0500

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -2,5 +2,5 @@
 
 uptime >> /data/log/jobmon.out
 echo "starting parsers and analysis" >>/data/log/jobmon.out
-perl /data/ldms/jobmon/bin/runparsers.pl &
-perl /data/ldms/jobmon/bin/runcheck.pl 
+perl /jobmon/bin/runparsers.pl &
+perl /jobmon/bin/runcheck.pl 
