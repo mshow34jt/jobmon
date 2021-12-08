fa7dfd4946852af7b4b9fd1f187f7971c7786a71
Mike Showerman <mshow@ncsa.illinois.edu>
2021-12-08 16:39:58 -0600

    Update load_procnetdev.sh

--- bin/load_procnetdev.sh
+++ bin/load_procnetdev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `find /data/ldms -maxdepth 1 -type f -name procnetdev.1*`
+for i in `find /data/ldms -maxdepth 1 -type f -name "procnetdev.1*"`
 do 
 	#echo $i
 	cat  $i |/jobmon/bin/parse_procnetdev.pl; 
