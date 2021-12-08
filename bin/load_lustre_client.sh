93f8f619195605a4693b4d31632246e28f07e2df
Mike Showerman <mshow@ncsa.illinois.edu>
2021-12-08 16:38:08 -0600

    Update load_lustre_client.sh

--- bin/load_lustre_client.sh
+++ bin/load_lustre_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `find /data/ldms -maxdepth 1 -type f -name lustre_client.1*`
+for i in `find /data/ldms -maxdepth 1 -type f -name "lustre_client.1*"`
 do 
 	#echo $i
 	cat  $i |/jobmon/bin/parse_lustre_client.pl; 
