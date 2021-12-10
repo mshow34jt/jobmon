b2230f3c1092cbc09c894d9713f10bad85ef89c4
Mike Showerman <mshow@ncsa.illinois.edu>
2021-12-09 23:48:11 -0600

    Create load_procnet.sh

---
+++ bin/load_procnet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in `find /data/ldms -maxdepth 1 -type f -name "procnet.1*"`
+do 
+	#echo $i
+	cat  $i |/jobmon/bin/parse_procnet.pl; 
+	mv  $i /data/ldms/bak  
+	mv  /data/ldms/procnet.HEADER*  2> /dev/null
+done
