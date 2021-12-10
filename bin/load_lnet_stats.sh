a348212eb7622bf4eaaba5509e3bdb22ff2d54b0
Mike Showerman <mshow@ncsa.illinois.edu>
2021-12-09 23:48:59 -0600

    Create load_lnet_stats.sh

---
+++ bin/load_lnet_stats.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in `find /data/ldms -maxdepth 1 -type f -name "lnet_stats.1*"`
+do 
+	#echo $i
+	cat  $i |/jobmon/bin/parse_lnet_stats.pl; 
+	mv  $i /data/ldms/bak  
+	mv  /data/ldms/lnet_stats.HEADER*  2> /dev/null
+done
