d78f68c3577b3ed403cc8d292c63cad7a5d15aa6
Mike Showerman <mshow@ncsa.illinois.edu>
2021-12-13 14:11:13 -0600

    Update runcheck.pl

--- bin/runcheck.pl
+++ bin/runcheck.pl
@@ -2,11 +2,11 @@
  
 while(1)
  {
-           `echo starting >> /data/log/jobmon.out`;
+           `echo starting >> /data/log/jobmon.log`;
            `uptime >> /data/log/jobmon.out`;
-           `/data/ldms/jobmon/bin/checkJobs.pl >> /data/log/jobmon.out`;
-           `echo finished >> /data/log/jobmon.out`;
-           `uptime >> /data/log/jobmon.out`;
+           `/jobmon/bin/checkJobs.pl >> /data/log/jobmon.log`;
+           `echo finished >> /data/log/jobmon.log`;
+           `uptime >> /data/log/jobmon.log`;
            sleep (120);
  
  }
